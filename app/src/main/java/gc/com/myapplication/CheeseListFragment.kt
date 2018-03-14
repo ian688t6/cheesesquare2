c0c52ed627773845dc3547356fc038c325ddcf83
ian <ian_yin@gcoreinc.com>
2018-03-14 17:43:40 +0800

    add CheeseListFragment

---
+++ app/src/main/java/gc/com/myapplication/CheeseListFragment.kt
@@ -0,0 +1,21 @@
+package gc.com.myapplication
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/**
+ * Created by jyin on 3/14/18.
+ */
+class CheeseListFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    private fun setRecycleView(recyclerView: RecyclerView) {
+
+    }
+}
